ec31dfb01ff4adf331c7c32d659160e6e7d0b1df
DESKTOP-6BHSFB1\Carl <carldrewx@gmail.com>
2022-04-26 18:09:00 +0800

    Updated attack script. Added a game object in main scene.

--- Project/Assets/Scripts/PlayerAttack.cs
+++ Project/Assets/Scripts/PlayerAttack.cs
@@ -4,13 +4,6 @@ using UnityEngine;
 
 public class PlayerAttack : MonoBehaviour
 {
-   /* [Header("Attack Attributes")]
-    [SerializeField] private Transform attackPos;
-    [SerializeField] private float attackRange;
-    [SerializeField] private int attackDamage;
-    [SerializeField] private float timeBtwAttack;
-    [SerializeField] private float startTimeBtwAttack;*/
-
     [Header("Components Reference")]
     [SerializeField] private Sword swordClass;
     [SerializeField] private Animator anim;
@@ -19,13 +12,10 @@ public class PlayerAttack : MonoBehaviour
     // Start is called before the first frame update
     private void Update()
     {
-            if(Input.GetKeyDown(KeyCode.J))
-            {
-                swordClass.AnimateSword();
-                swordClass.DamageEnemy();
-                //GetComponent<Sword>().AnimateSword();
-                //GetComponent<Sword>().DamageEnemy();
-            }
-
+        if(Input.GetKeyDown(KeyCode.J))
+        {
+            swordClass.AnimateSword();
+            swordClass.DamageEnemy();
+        }
     }
 }
